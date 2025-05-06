2e2767618251745b69f8722e132377db2f811aa2
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-05-06 16:23:13 +0100

    Removed Identity.Api project

--- src/Identity.Api/Models/ApplicationUser.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace Identity.Api.Models;
-
-internal sealed class ApplicationUser : IdentityUser
-{
-    public string Name { get; set; } = string.Empty;
-}

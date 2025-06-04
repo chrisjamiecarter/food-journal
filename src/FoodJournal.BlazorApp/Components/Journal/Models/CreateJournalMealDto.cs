5ece3dd7e9a2583199133dedbaf1be25ac363aee
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-06-04 17:30:26 +0100

    Add meal deletion functionality and DTOs
    - Updated `CreateJournalMealDto` to use `MealType` enum.
    - Introduced `DeleteJournalMealFoodDto` for food deletion.
    - Modified `Index.razor` to handle food deletion with new event callback.
    - Added `HandleDeleteMealFood` method for processing deletions.
    - Enhanced `JournalMeal.razor` with button for deleting food items.
    - Updated `JournalMeal` component to communicate deletions.
    - Implemented `HandleDeleteJournalMealFood` method for deletion logic.

--- src/FoodJournal.BlazorApp/Components/Journal/Models/CreateJournalMealDto.cs
+++ src/FoodJournal.BlazorApp/Components/Journal/Models/CreateJournalMealDto.cs
@@ -6,7 +6,6 @@ namespace FoodJournal.BlazorApp.Components.Journal.Models;
 
 public class CreateJournalMealDto
 {
-    public Guid? Id { get; set; }
     public DateTime Date { get; set; } = DateTime.Now;
     public MealType Type { get; set; } = MealType.Breakfast;
     public Collection<Food> Foods { get; } = [];
---
+++ src/FoodJournal.BlazorApp/Components/Journal/Models/DeleteJournalMealFoodDto.cs
@@ -0,0 +1,10 @@
+﻿using FoodJournal.Application.Enums;
+
+namespace FoodJournal.BlazorApp.Components.Journal.Models;
+
+public class DeleteJournalMealFoodDto
+{
+    public DateTime Date { get; set; } = DateTime.Now;
+    public MealType Type { get; set; } = MealType.Breakfast;
+    public Guid FoodId { get; set; }
+}

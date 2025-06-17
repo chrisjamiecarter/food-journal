918110566d586f8494f5353fdc5e4bcffed41e4f
Chris Carter <108958713+chrisjamiecarter@users.noreply.github.com>
2025-06-17 18:40:47 +0100

    Added validation to the create meal input model

--- src/FoodJournal.BlazorApp/Components/Meals/Models/CreateMealInputModel.cs
+++ src/FoodJournal.BlazorApp/Components/Meals/Models/CreateMealInputModel.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
 using FoodJournal.Application.Entities;
 
 namespace FoodJournal.BlazorApp.Components.Meals.Models;
 
-public class CreateMealDto
+public class CreateMealInputModel
 {
+    [Required(ErrorMessage = "Please enter a meal name.")]
+    [DataType(DataType.Text)]
     public string Name { get; set; } = string.Empty;
+
+    [MinLength(1, ErrorMessage = "Please select at least one food item.")]
     public Collection<Food> Foods { get; } = [];
 }
